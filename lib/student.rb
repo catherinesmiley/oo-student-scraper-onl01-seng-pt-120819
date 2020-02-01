b1b403996cef64376e472489cf407a067db48571
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 16:32:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     binding.pry 
-    students_array.each {|student| student = Student.new}
+    students_array.each {|student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)

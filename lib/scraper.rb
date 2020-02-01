a1ec2d91a78da0fa32810ba772c7ec5d8cb13ae6
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 15:54:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
     student_profile = {}
     html = open(index_url)
     index = Nokogiri::HTML(html)
-    student_profile[:twitter] = index.css(div.("a").attribute("href").value 
+    student_profile[:twitter] = index.css(div.social("a").attribute("href").value 
   end
 
 end

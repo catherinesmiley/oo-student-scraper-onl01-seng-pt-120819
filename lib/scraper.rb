1aa814357685ec79c642325808d7db48e7d721cc
Catherine Smiley <catherinesmiley@gmail.com>
2020-02-01 16:09:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
     student_profile = {}
     html = open(profile_url)
     profile = Nokogiri::HTML(html)
-    profile.css("div.main-wrapper.profile social-icon-container").each do |social_media|      
+    profile.css("div.main-wrapper.profile .social-icon-container").each do |social_media|      
       if social_media.attribute("href").value.include?("twitter")
         student_profile[:twitter] = social_media.css("a").attribute("href").value 
       elsif social_media.attribute("href").value.include?("linkedin")
